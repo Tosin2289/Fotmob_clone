45ac79caaf0856f5cd89fee462ae4ef8c817a484
Tosin2289 <ayoolatosin00@gmail.com>
2023-08-12 22:00:22 +0100

    second commit

--- lib/main.dart
+++ lib/main.dart
@@ -11,9 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.green,
       ),
       home: const HomeScreen(),
     );
