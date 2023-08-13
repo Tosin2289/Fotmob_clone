3e3732416421feba09ddab0d0629802db5098e52
Tosin2289 <ayoolatosin00@gmail.com>
2023-08-13 23:30:50 +0100

    second commit

--- lib/pages/HomeScreen/homescreen.dart
+++ lib/pages/HomeScreen/homescreen.dart
@@ -5,6 +5,29 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.arrow_back_ios),
+        centerTitle: true,
+        title: const Text(
+          "FOTPAL",
+          style: TextStyle(fontSize: 25, fontWeight: FontWeight.w400),
+        ),
+        actions: const [
+          Icon(
+            Icons.notifications_outlined,
+            size: 15,
+          ),
+          Icon(
+            Icons.share_outlined,
+            size: 15,
+          ),
+          Icon(
+            Icons.more_vert,
+            size: 15,
+          ),
+        ],
+      ),
+    );
   }
 }
