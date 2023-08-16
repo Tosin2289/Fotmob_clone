8af9484d017e21005bde59f1d1fcf9c00ff1b3b3
Tosin2289 <ayoolatosin00@gmail.com>
2023-08-16 21:54:28 +0100

    third commit

--- lib/pages/HomeScreen/homescreen.dart
+++ lib/pages/HomeScreen/homescreen.dart
@@ -7,48 +7,107 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: const Icon(Icons.arrow_back_ios),
+        backgroundColor: Colors.transparent,
+        leading: const Icon(
+          Icons.arrow_back_ios,
+          color: Colors.black,
+        ),
+        elevation: 0,
         centerTitle: true,
         title: const Text(
           "FOTPAL",
-          style: TextStyle(fontSize: 25, fontWeight: FontWeight.w400),
+          style: TextStyle(
+              fontSize: 25, fontWeight: FontWeight.w500, color: Colors.black),
         ),
         actions: const [
           Icon(
             Icons.notifications_outlined,
-            size: 15,
+            color: Colors.black,
+            size: 28,
+          ),
+          SizedBox(
+            width: 10,
           ),
           Icon(
             Icons.share_outlined,
-            size: 15,
+            color: Colors.black,
+            size: 28,
+          ),
+          SizedBox(
+            width: 10,
           ),
           Icon(
             Icons.more_vert,
-            size: 15,
+            color: Colors.black,
+            size: 28,
           ),
         ],
       ),
       body: Column(
         children: [
           Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
             children: [
               Container(
-                height: 50,
-                width: 50,
+                height: 40,
+                width: 60,
                 decoration: BoxDecoration(
                   color: Colors.grey.shade200,
                   borderRadius: BorderRadius.circular(10),
                 ),
                 child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
                     Container(
-                      height: 10,
+                      height: 30,
+                      width: 10,
                       decoration: const BoxDecoration(
                         shape: BoxShape.circle,
                         color: Colors.green,
                       ),
+                    ),
+                    const Text(
+                      "Live",
+                      style: TextStyle(color: Colors.grey, fontSize: 20),
                     )
+                  ],
+                ),
+              ),
+              SizedBox(
+                height: 30,
+                width: 60,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: const [
+                    Text(
+                      "Today",
+                      style: TextStyle(color: Colors.black, fontSize: 15),
+                    ),
+                    Text(
+                      "15 Apr",
+                      style: TextStyle(color: Colors.grey, fontSize: 10),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                height: 40,
+                width: 60,
+                decoration: BoxDecoration(
+                  color: Colors.green[700],
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: const [
+                    Text(
+                      "FRI",
+                      style: TextStyle(color: Colors.white, fontSize: 15),
+                    ),
+                    Text(
+                      "16 Apr",
+                      style: TextStyle(color: Colors.white, fontSize: 10),
+                    ),
                   ],
                 ),
               ),
