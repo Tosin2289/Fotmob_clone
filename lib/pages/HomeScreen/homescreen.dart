77d662b6612199df942797032b5212678f552bb7
Tosin2289 <ayoolatosin00@gmail.com>
2023-08-17 23:07:53 +0100

    fourth commit

--- lib/pages/HomeScreen/homescreen.dart
+++ lib/pages/HomeScreen/homescreen.dart
@@ -46,107 +46,111 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Column(
         children: [
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            children: [
-              Container(
-                height: 40,
-                width: 60,
-                decoration: BoxDecoration(
-                  color: Colors.grey.shade200,
-                  borderRadius: BorderRadius.circular(10),
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    Container(
-                      height: 30,
-                      width: 10,
-                      decoration: const BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.green,
+          Container(
+            height: 100,
+            decoration: const BoxDecoration(color: Colors.white),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  height: 40,
+                  width: 60,
+                  decoration: BoxDecoration(
+                    color: Colors.grey.shade200,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Container(
+                        height: 30,
+                        width: 10,
+                        decoration: const BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: Colors.green,
+                        ),
                       ),
-                    ),
-                    const Text(
-                      "Live",
-                      style: TextStyle(color: Colors.grey, fontSize: 20),
-                    )
-                  ],
+                      const Text(
+                        "Live",
+                        style: TextStyle(color: Colors.grey, fontSize: 20),
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 30,
-                width: 60,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: const [
-                    Text(
-                      "Today",
-                      style: TextStyle(color: Colors.black, fontSize: 15),
-                    ),
-                    Text(
-                      "15 Apr",
-                      style: TextStyle(color: Colors.grey, fontSize: 10),
-                    ),
-                  ],
+                SizedBox(
+                  height: 30,
+                  width: 60,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: const [
+                      Text(
+                        "Today",
+                        style: TextStyle(color: Colors.black, fontSize: 15),
+                      ),
+                      Text(
+                        "15 Apr",
+                        style: TextStyle(color: Colors.grey, fontSize: 10),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              Container(
-                height: 35,
-                width: 55,
-                decoration: BoxDecoration(
-                  color: Colors.green[800],
-                  borderRadius: BorderRadius.circular(8),
+                Container(
+                  height: 35,
+                  width: 55,
+                  decoration: BoxDecoration(
+                    color: Colors.green[800],
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: const [
+                      Text(
+                        "FRI",
+                        style: TextStyle(color: Colors.white, fontSize: 18),
+                      ),
+                      Text(
+                        "16 Apr",
+                        style: TextStyle(color: Colors.white, fontSize: 12),
+                      ),
+                    ],
+                  ),
                 ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: const [
-                    Text(
-                      "FRI",
-                      style: TextStyle(color: Colors.white, fontSize: 18),
-                    ),
-                    Text(
-                      "16 Apr",
-                      style: TextStyle(color: Colors.white, fontSize: 12),
-                    ),
-                  ],
+                SizedBox(
+                  height: 30,
+                  width: 60,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: const [
+                      Text(
+                        "SAT",
+                        style: TextStyle(color: Colors.black, fontSize: 15),
+                      ),
+                      Text(
+                        "17 Apr",
+                        style: TextStyle(color: Colors.grey, fontSize: 10),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 30,
-                width: 60,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: const [
-                    Text(
-                      "SAT",
-                      style: TextStyle(color: Colors.black, fontSize: 15),
-                    ),
-                    Text(
-                      "17 Apr",
-                      style: TextStyle(color: Colors.grey, fontSize: 10),
-                    ),
-                  ],
+                SizedBox(
+                  height: 30,
+                  width: 60,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: const [
+                      Text(
+                        "SUN",
+                        style: TextStyle(color: Colors.black, fontSize: 15),
+                      ),
+                      Text(
+                        "18 Apr",
+                        style: TextStyle(color: Colors.grey, fontSize: 10),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
-              SizedBox(
-                height: 30,
-                width: 60,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: const [
-                    Text(
-                      "SUN",
-                      style: TextStyle(color: Colors.black, fontSize: 15),
-                    ),
-                    Text(
-                      "18 Apr",
-                      style: TextStyle(color: Colors.grey, fontSize: 10),
-                    ),
-                  ],
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
           const SizedBox(
             height: 10,
@@ -171,6 +175,10 @@ class HomeScreen extends StatelessWidget {
               ],
             ),
           ),
+          Expanded(
+              child: Container(
+            color: Colors.white,
+          ))
         ],
       ),
     );
