0ed068902e7dfeb3b61263a2bdcf2ca61df580e1
Tosin2289 <ayoolatosin00@gmail.com>
2023-08-20 22:59:57 +0100

    fourth commit

--- lib/pages/HomeScreen/homescreen.dart
+++ lib/pages/HomeScreen/homescreen.dart
@@ -44,242 +44,246 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Container(
-            height: 100,
-            decoration: const BoxDecoration(color: Colors.white),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                Container(
-                  height: 39,
-                  width: 59,
-                  decoration: BoxDecoration(
-                    color: Colors.grey.shade200,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Container(
-                        height: 30,
-                        width: 10,
-                        decoration: const BoxDecoration(
-                          shape: BoxShape.circle,
-                          color: Colors.green,
-                        ),
-                      ),
-                      const Text(
-                        "Live",
-                        style: TextStyle(color: Colors.grey, fontSize: 20),
-                      )
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 30,
-                  width: 60,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: const [
-                      Text(
-                        "Today",
-                        style: TextStyle(color: Colors.black, fontSize: 15),
-                      ),
-                      Text(
-                        "15 Apr",
-                        style: TextStyle(color: Colors.grey, fontSize: 10),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  height: 35,
-                  width: 55,
-                  decoration: BoxDecoration(
-                    color: Colors.green[900],
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: const [
-                      Text(
-                        "FRI",
-                        style: TextStyle(color: Colors.white, fontSize: 18),
-                      ),
-                      Text(
-                        "16 Apr",
-                        style: TextStyle(color: Colors.white, fontSize: 12),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 30,
-                  width: 60,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: const [
-                      Text(
-                        "SAT",
-                        style: TextStyle(color: Colors.black, fontSize: 15),
-                      ),
-                      Text(
-                        "17 Apr",
-                        style: TextStyle(color: Colors.grey, fontSize: 10),
-                      ),
-                    ],
-                  ),
-                ),
-                SizedBox(
-                  height: 30,
-                  width: 60,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: const [
-                      Text(
-                        "SUN",
-                        style: TextStyle(color: Colors.black, fontSize: 15),
-                      ),
-                      Text(
-                        "18 Apr",
-                        style: TextStyle(color: Colors.grey, fontSize: 10),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-          const SizedBox(
-            height: 10,
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 12.0),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: const [
-                Text(
-                  "Champions League",
-                  style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black),
-                ),
-                Icon(
-                  Icons.arrow_drop_up,
-                  color: Colors.black,
-                  size: 30,
-                )
-              ],
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Expanded(
-              child: Container(
-                color: Colors.white,
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.vertical,
-                  child: Column(
-                    children: [
-                      Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.end,
-                              children: [
-                                Text(
-                                  "Table",
-                                  style: TextStyle(
-                                      color: Colors.green.shade800,
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                                const SizedBox(
-                                  width: 5,
-                                ),
-                                const Icon(
-                                  Icons.arrow_forward_ios,
-                                  color: Colors.green,
-                                )
-                              ],
-                            ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              height: 100,
+              decoration: const BoxDecoration(color: Colors.white),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    height: 39,
+                    width: 59,
+                    decoration: BoxDecoration(
+                      color: Colors.grey.shade200,
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        Container(
+                          height: 30,
+                          width: 10,
+                          decoration: const BoxDecoration(
+                            shape: BoxShape.circle,
+                            color: Colors.green,
                           ),
-                          const Gamecard(
-                              date: "29 APR",
-                              time: "8:00 PM",
-                              logo1: "assets/manutd.png",
-                              logo2: "assets/chelsea.png",
-                              team1: "Manchester United",
-                              team2: "Chelsea"),
-                          Divider(),
-                          const Gamecard(
-                              date: "12 JUN",
-                              time: "1:00 PM",
-                              logo1: "assets/madrid.png",
-                              logo2: "assets/dortmound.png",
-                              team1: "Real Madrid",
-                              team2: "Dortmound"),
-                          Divider(),
-                          const Gamecard(
-                              date: "29 APR",
-                              time: "8:00 PM",
-                              logo1: "assets/roma.png",
-                              logo2: "assets/lazio.png",
-                              team1: "A.S.Roma",
-                              team2: "Lazio"),
-                          Divider(),
-                          const Gamecard(
-                              date: "29 APR",
-                              time: "8:00 PM",
-                              logo1: "assets/sevilla.png",
-                              logo2: "assets/betis.png",
-                              team1: "Sevilla",
-                              team2: "Real Betis"),
-                          Divider(),
-                          const Gamecard(
-                              date: "29 APR",
-                              time: "8:00 PM",
-                              logo1: "assets/madrid.png",
-                              logo2: "assets/barca.png",
-                              team1: "Real Madrid",
-                              team2: "Barcelona"),
-                          Divider(),
-                          const Gamecard(
-                              date: "29 APR",
-                              time: "8:00 PM",
-                              logo1: "assets/valencia.png",
-                              logo2: "assets/chelsea.png",
-                              team1: "Valencia",
-                              team2: "Chelsea"),
-                          Divider(),
-                          const Gamecard(
-                              date: "12 JUN",
-                              time: "1:00 PM",
-                              logo1: "assets/madrid.png",
-                              logo2: "assets/dortmound.png",
-                              team1: "Real Madrid",
-                              team2: "Dortmound"),
-                          Divider(),
-                          const Gamecard(
-                              date: "29 APR",
-                              time: "8:00 PM",
-                              logo1: "assets/valencia.png",
-                              logo2: "assets/chelsea.png",
-                              team1: "Valencia",
-                              team2: "Chelsea"),
+                        ),
+                        const Text(
+                          "Live",
+                          style: TextStyle(color: Colors.grey, fontSize: 20),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                    width: 60,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: const [
+                        Text(
+                          "Today",
+                          style: TextStyle(color: Colors.black, fontSize: 15),
+                        ),
+                        Text(
+                          "15 Apr",
+                          style: TextStyle(color: Colors.grey, fontSize: 10),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    height: 35,
+                    width: 55,
+                    decoration: BoxDecoration(
+                      color: Colors.green[900],
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: const [
+                        Text(
+                          "FRI",
+                          style: TextStyle(color: Colors.white, fontSize: 18),
+                        ),
+                        Text(
+                          "16 Apr",
+                          style: TextStyle(color: Colors.white, fontSize: 12),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                    width: 60,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: const [
+                        Text(
+                          "SAT",
+                          style: TextStyle(color: Colors.black, fontSize: 15),
+                        ),
+                        Text(
+                          "17 Apr",
+                          style: TextStyle(color: Colors.grey, fontSize: 10),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 30,
+                    width: 60,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: const [
+                        Text(
+                          "SUN",
+                          style: TextStyle(color: Colors.black, fontSize: 15),
+                        ),
+                        Text(
+                          "18 Apr",
+                          style: TextStyle(color: Colors.grey, fontSize: 10),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: const [
+                  Text(
+                    "Champions League",
+                    style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black),
+                  ),
+                  Icon(
+                    Icons.arrow_drop_up,
+                    color: Colors.black,
+                    size: 30,
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                child: Expanded(
+                  child: Container(
+                    color: Colors.white,
+                    child: SingleChildScrollView(
+                      scrollDirection: Axis.vertical,
+                      child: Column(
+                        children: [
+                          Column(
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.end,
+                                  children: [
+                                    Text(
+                                      "Table",
+                                      style: TextStyle(
+                                          color: Colors.green.shade800,
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    const SizedBox(
+                                      width: 5,
+                                    ),
+                                    const Icon(
+                                      Icons.arrow_forward_ios,
+                                      color: Colors.green,
+                                    )
+                                  ],
+                                ),
+                              ),
+                              const Gamecard(
+                                  date: "29 APR",
+                                  time: "8:00 PM",
+                                  logo1: "assets/manutd.png",
+                                  logo2: "assets/chelsea.png",
+                                  team1: "Manchester United",
+                                  team2: "Chelsea"),
+                              const Divider(),
+                              const Gamecard(
+                                  date: "12 JUN",
+                                  time: "1:00 PM",
+                                  logo1: "assets/madrid.png",
+                                  logo2: "assets/dortmound.png",
+                                  team1: "Real Madrid",
+                                  team2: "Dortmound"),
+                              const Divider(),
+                              const Gamecard(
+                                  date: "29 APR",
+                                  time: "8:00 PM",
+                                  logo1: "assets/roma.png",
+                                  logo2: "assets/lazio.png",
+                                  team1: "A.S.Roma",
+                                  team2: "Lazio"),
+                              Divider(),
+                              const Gamecard(
+                                  date: "29 APR",
+                                  time: "8:00 PM",
+                                  logo1: "assets/sevilla.png",
+                                  logo2: "assets/betis.png",
+                                  team1: "Sevilla",
+                                  team2: "Real Betis"),
+                              const Divider(),
+                              const Gamecard(
+                                  date: "29 APR",
+                                  time: "8:00 PM",
+                                  logo1: "assets/madrid.png",
+                                  logo2: "assets/barca.png",
+                                  team1: "Real Madrid",
+                                  team2: "Barcelona"),
+                              Divider(),
+                              const Gamecard(
+                                  date: "29 APR",
+                                  time: "8:00 PM",
+                                  logo1: "assets/valencia.png",
+                                  logo2: "assets/chelsea.png",
+                                  team1: "Valencia",
+                                  team2: "Chelsea"),
+                              const Divider(),
+                              const Gamecard(
+                                  date: "12 JUN",
+                                  time: "1:00 PM",
+                                  logo1: "assets/madrid.png",
+                                  logo2: "assets/dortmound.png",
+                                  team1: "Real Madrid",
+                                  team2: "Dortmound"),
+                              const Divider(),
+                              const Gamecard(
+                                  date: "29 APR",
+                                  time: "8:00 PM",
+                                  logo1: "assets/valencia.png",
+                                  logo2: "assets/chelsea.png",
+                                  team1: "Valencia",
+                                  team2: "Chelsea"),
+                            ],
+                          ),
                         ],
                       ),
-                    ],
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
