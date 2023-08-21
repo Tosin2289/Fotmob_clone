9177bf4043ce27d9342388d7648b0a9eb0e9a57d
Tosin2289 <ayoolatosin00@gmail.com>
2023-08-21 22:52:41 +0100

    fifth commit

--- lib/pages/HomeScreen/homescreen.dart
+++ lib/pages/HomeScreen/homescreen.dart
@@ -293,11 +293,13 @@ class Gamecard extends StatelessWidget {
   final String date;
   final String time;
   final logo1;
+  final ontap;
   final logo2;
   final String team1;
   final String team2;
   const Gamecard({
     super.key,
+    required this.ontap,
     required this.date,
     required this.time,
     required this.logo1,
@@ -308,92 +310,95 @@ class Gamecard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 80,
-      child: Row(
-        children: [
-          Expanded(
-              flex: 2,
-              child: SizedBox(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 12.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                        date,
-                        style:
-                            const TextStyle(fontSize: 16, color: Colors.grey),
-                      ),
-                      Text(
-                        time,
-                        style:
-                            const TextStyle(fontSize: 16, color: Colors.grey),
-                      ),
-                    ],
+    return GestureDetector(
+      onTap: ontap,
+      child: SizedBox(
+        height: 80,
+        child: Row(
+          children: [
+            Expanded(
+                flex: 2,
+                child: SizedBox(
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 12.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Text(
+                          date,
+                          style:
+                              const TextStyle(fontSize: 16, color: Colors.grey),
+                        ),
+                        Text(
+                          time,
+                          style:
+                              const TextStyle(fontSize: 16, color: Colors.grey),
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              )),
-          Expanded(
-              flex: 5,
-              child: SizedBox(
-                child: Padding(
+                )),
+            Expanded(
+                flex: 5,
+                child: SizedBox(
+                  child: Padding(
+                    padding: const EdgeInsets.all(12.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Image.asset(
+                              logo1,
+                              height: 25,
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              team1,
+                              style: const TextStyle(fontSize: 18),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Image.asset(
+                              logo2,
+                              height: 25,
+                            ),
+                            const SizedBox(
+                              width: 10,
+                            ),
+                            Text(
+                              team2,
+                              style: const TextStyle(fontSize: 18),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                )),
+            Expanded(
+                flex: 1,
+                child: SizedBox(
+                    child: Padding(
                   padding: const EdgeInsets.all(12.0),
                   child: Column(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          Image.asset(
-                            logo1,
-                            height: 25,
-                          ),
-                          const SizedBox(
-                            width: 10,
-                          ),
-                          Text(
-                            team1,
-                            style: const TextStyle(fontSize: 18),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Image.asset(
-                            logo2,
-                            height: 25,
-                          ),
-                          const SizedBox(
-                            width: 10,
-                          ),
-                          Text(
-                            team2,
-                            style: const TextStyle(fontSize: 18),
-                          ),
-                        ],
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Icon(
+                        Icons.star_border_outlined,
+                        color: Colors.grey,
+                        size: 35,
                       ),
                     ],
                   ),
-                ),
-              )),
-          Expanded(
-              flex: 1,
-              child: SizedBox(
-                  child: Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    Icon(
-                      Icons.star_border_outlined,
-                      color: Colors.grey,
-                      size: 35,
-                    ),
-                  ],
-                ),
-              ))),
-        ],
+                ))),
+          ],
+        ),
       ),
     );
   }
