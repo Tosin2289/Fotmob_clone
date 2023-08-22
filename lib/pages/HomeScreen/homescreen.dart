7ebdd536b42d32da0469fc94faae7073801bfa95
Tosin2289 <ayoolatosin00@gmail.com>
2023-08-22 23:20:15 +0100

    fifth commit

--- lib/pages/HomeScreen/homescreen.dart
+++ lib/pages/HomeScreen/homescreen.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:fotmob_clone/pages/HomeScreen/detailsscreen.dart';
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+import '../../componets/matchcard.dart';
+import '../../models/matchmodel.dart';
 
+class HomeScreen extends StatelessWidget {
+  HomeScreen({Key? key}) : super(key: key);
+  final List<MatchModel> matchdata = [];
   @override
   Widget build(BuildContext context) {
     void navigate() {
@@ -299,121 +302,6 @@ class HomeScreen extends StatelessWidget {
                 ),
               ),
             ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class Gamecard extends StatelessWidget {
-  final String date;
-  final String time;
-  final logo1;
-  final ontap;
-  final logo2;
-  final String team1;
-  final String team2;
-  const Gamecard({
-    super.key,
-    required this.ontap,
-    required this.date,
-    required this.time,
-    required this.logo1,
-    required this.logo2,
-    required this.team1,
-    required this.team2,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: ontap,
-      child: SizedBox(
-        height: 80,
-        child: Row(
-          children: [
-            Expanded(
-                flex: 2,
-                child: SizedBox(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 12.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(
-                          date,
-                          style:
-                              const TextStyle(fontSize: 16, color: Colors.grey),
-                        ),
-                        Text(
-                          time,
-                          style:
-                              const TextStyle(fontSize: 16, color: Colors.grey),
-                        ),
-                      ],
-                    ),
-                  ),
-                )),
-            Expanded(
-                flex: 5,
-                child: SizedBox(
-                  child: Padding(
-                    padding: const EdgeInsets.all(12.0),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Row(
-                          children: [
-                            Image.asset(
-                              logo1,
-                              height: 25,
-                            ),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            Text(
-                              team1,
-                              style: const TextStyle(fontSize: 18),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          children: [
-                            Image.asset(
-                              logo2,
-                              height: 25,
-                            ),
-                            const SizedBox(
-                              width: 10,
-                            ),
-                            Text(
-                              team2,
-                              style: const TextStyle(fontSize: 18),
-                            ),
-                          ],
-                        ),
-                      ],
-                    ),
-                  ),
-                )),
-            Expanded(
-                flex: 1,
-                child: SizedBox(
-                    child: Padding(
-                  padding: const EdgeInsets.all(12.0),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Icon(
-                        Icons.star_border_outlined,
-                        color: Colors.grey,
-                        size: 35,
-                      ),
-                    ],
-                  ),
-                ))),
           ],
         ),
       ),
