87b8fb5298323d6e41f72266e513d409123de62e
Tosin2289 <ayoolatosin00@gmail.com>
2023-08-23 20:57:58 +0100

    fifith commit

--- lib/pages/HomeScreen/detailsscreen.dart
+++ lib/pages/HomeScreen/detailsscreen.dart
@@ -12,9 +12,56 @@ class DetailsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey.shade100,
       appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: const Icon(
+          Icons.arrow_back_ios,
+          color: Colors.black,
+        ),
+        elevation: 0,
         centerTitle: true,
-        title: Text("$team1 vs $team2"),
+        title: const Text(
+          "FOTPAL",
+          style: TextStyle(
+              fontSize: 25, fontWeight: FontWeight.w500, color: Colors.black),
+        ),
+        actions: const [
+          Icon(
+            Icons.notifications_outlined,
+            color: Colors.black,
+            size: 28,
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Icon(
+            Icons.share_outlined,
+            color: Colors.black,
+            size: 28,
+          ),
+          SizedBox(
+            width: 10,
+          ),
+          Icon(
+            Icons.more_vert,
+            color: Colors.black,
+            size: 28,
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          Container(
+            height: 300,
+            decoration: const BoxDecoration(color: Colors.white),
+            child: Center(
+              child: Column(
+                children: [],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
