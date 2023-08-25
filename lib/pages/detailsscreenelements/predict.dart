794a1ddbe94e41f11a6acccfb221633f87ea85d8
Tosin2289 <ayoolatosin00@gmail.com>
2023-08-25 22:16:52 +0100

    sixth commit

--- lib/pages/detailsscreenelements/predict.dart
+++ lib/pages/detailsscreenelements/predict.dart
@@ -21,8 +21,120 @@ class Predict extends StatelessWidget {
                 Text(
                   "Leader Board",
                   style: TextStyle(color: Colors.green, fontSize: 16),
-                )
+                ),
               ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 12.0),
+            child: Container(
+              decoration: BoxDecoration(
+                  color: Colors.white, borderRadius: BorderRadius.circular(10)),
+              height: 155,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Row(
+                      children: const [
+                        Text(
+                          "Who do you think will win ?",
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        SizedBox(
+                          height: 100,
+                          child: Column(
+                            children: [
+                              Container(
+                                height: 50,
+                                width: 50,
+                                decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.black26),
+                                    borderRadius: BorderRadius.circular(10)),
+                                child: const Center(
+                                    child: Text(
+                                  "1",
+                                  style: TextStyle(fontSize: 18),
+                                )),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              const Text(
+                                "Team1",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500, fontSize: 14),
+                              )
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 100,
+                          child: Column(
+                            children: [
+                              Container(
+                                height: 50,
+                                width: 50,
+                                decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.black26),
+                                    borderRadius: BorderRadius.circular(10)),
+                                child: const Center(
+                                    child: Text(
+                                  "x",
+                                  style: TextStyle(fontSize: 18),
+                                )),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              const Text(
+                                "Draw",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500, fontSize: 14),
+                              )
+                            ],
+                          ),
+                        ),
+                        SizedBox(
+                          height: 100,
+                          child: Column(
+                            children: [
+                              Container(
+                                height: 50,
+                                width: 50,
+                                decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.black26),
+                                    borderRadius: BorderRadius.circular(10)),
+                                child: const Center(
+                                    child: Text(
+                                  "2",
+                                  style: TextStyle(fontSize: 18),
+                                )),
+                              ),
+                              const SizedBox(
+                                height: 10,
+                              ),
+                              const Text(
+                                "Team2",
+                                style: TextStyle(
+                                    fontWeight: FontWeight.w500, fontSize: 14),
+                              )
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
             ),
           ),
         ],
