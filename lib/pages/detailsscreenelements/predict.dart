868984343f3a2958c0c5a9300f00e000efbad527
Tosin2289 <ayoolatosin00@gmail.com>
2023-08-25 21:43:35 +0100

    sixth commit

---
+++ lib/pages/detailsscreenelements/predict.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Predict extends StatelessWidget {
+  const Predict({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
