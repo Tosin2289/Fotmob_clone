7b7134f4ead126abf89388214bf92fd324357527
Tosin2289 <ayoolatosin00@gmail.com>
2023-08-29 18:28:34 +0100

    eighth commit

--- lib/pages/detailsscreenelements/h2h.dart
+++ lib/pages/detailsscreenelements/h2h.dart
@@ -35,12 +35,119 @@ class H2H extends StatelessWidget {
             height: 10,
           ),
           Container(
-            height: 150,
+            height: 160,
             decoration: BoxDecoration(
                 borderRadius: BorderRadius.circular(10), color: Colors.white),
-          ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Image.asset(
+                        logo1,
+                        height: 50,
+                      ),
+                      const Text("Previous Matches(17)",
+                          style: TextStyle(color: Colors.grey, fontSize: 18)),
+                      Image.asset(
+                        logo2,
+                        height: 50,
+                      ),
+                    ],
+                  ),
+                  const Divider(
+                    thickness: .8,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      hth(
+                        Color: color1,
+                        text: "4",
+                        outcome: "Won",
+                        percent: "24%",
+                      ),
+                      hth(
+                        Color: Colors.grey.shade300,
+                        text: "4",
+                        outcome: "Draw",
+                        percent: "24%",
+                      ),
+                      hth(
+                        Color: color2,
+                        text: "9",
+                        outcome: "Won",
+                        percent: "53%",
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          )
         ]),
       ),
+    );
+  }
+}
+
+class hth extends StatelessWidget {
+  final Color;
+  final String text;
+  final String outcome;
+  final String percent;
+  const hth({
+    super.key,
+    required this.Color,
+    required this.text,
+    required this.outcome,
+    required this.percent,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          height: 60,
+          width: 55,
+          decoration: BoxDecoration(
+              color: Color, borderRadius: BorderRadius.circular(8)),
+          child: Center(
+            child: Text(
+              text,
+              style: const TextStyle(color: Colors.white, fontSize: 28),
+            ),
+          ),
+        ),
+        const SizedBox(
+          width: 4,
+        ),
+        SizedBox(
+          height: 60,
+          width: 55,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                outcome,
+                style:
+                    const TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
+              ),
+              Text(
+                percent,
+                style: const TextStyle(color: Colors.grey, fontSize: 22),
+              )
+            ],
+          ),
+        )
+      ],
     );
   }
 }
